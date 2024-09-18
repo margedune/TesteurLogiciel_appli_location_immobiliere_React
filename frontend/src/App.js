d00c6aa12a9308c8f3e6ffbd66a450ea63ea02db
Rova RAHAROSON <margedune@gmail.com>
2024-09-18 07:05:34 +0100

    mise en page de la navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
+import Navigation from './Navigation';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navigation />
       <AppRoutes />
     </Router>
   );
---
+++ frontend/src/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Navigation = () => {
+    return (
+        <nav>
+            <img src="/images/logo.png"></img>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
